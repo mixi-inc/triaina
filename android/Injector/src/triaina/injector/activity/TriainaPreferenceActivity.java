cabd60d4557717aded385961d728b1afc49ef430
Hirofumi Nakagawa <hirofumi.nakagawa@mixi.co.jp>
2013-03-14 08:17:59 +0900

    Add a TriainaPreferenceActivity class

---
+++ android/Injector/src/triaina/injector/activity/TriainaPreferenceActivity.java
@@ -0,0 +1,135 @@
+package triaina.injector.activity;
+
+import java.util.HashMap;
+
+import javax.inject.Inject;
+
+import com.google.inject.Key;
+
+import roboguice.activity.event.OnActivityResultEvent;
+import roboguice.activity.event.OnConfigurationChangedEvent;
+import roboguice.activity.event.OnContentChangedEvent;
+import roboguice.activity.event.OnCreateEvent;
+import roboguice.activity.event.OnNewIntentEvent;
+import roboguice.activity.event.OnPauseEvent;
+import roboguice.activity.event.OnRestartEvent;
+import roboguice.activity.event.OnResumeEvent;
+import roboguice.activity.event.OnStopEvent;
+import roboguice.event.EventManager;
+import roboguice.inject.ContentViewListener;
+import roboguice.inject.ContextScope;
+import roboguice.inject.PreferenceListener;
+import roboguice.service.event.OnDestroyEvent;
+import roboguice.service.event.OnStartEvent;
+import triaina.injector.TriainaInjector;
+import triaina.injector.TriainaInjectorFactory;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
+
+public class TriainaPreferenceActivity extends PreferenceActivity {
+    protected EventManager eventManager;
+    protected PreferenceListener preferenceListener;
+
+    @Inject ContentViewListener ignored; // BUG find a better place to put this
+
+    /** {@inheritDoc } */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        final TriainaInjector injector = TriainaInjectorFactory.getInjector(this);
+        eventManager = injector.getInstance(EventManager.class);
+        preferenceListener = injector.getInstance(PreferenceListener.class);
+        injector.injectMembersWithoutViews(this);
+        super.onCreate(savedInstanceState);
+        eventManager.fire(new OnCreateEvent(savedInstanceState));
+    }
+
+    @Override
+    public void setPreferenceScreen(PreferenceScreen preferenceScreen) {
+        super.setPreferenceScreen(preferenceScreen);
+
+        final ContextScope scope = TriainaInjectorFactory.getInjector(this).getInstance(ContextScope.class);
+        synchronized (ContextScope.class) {
+            scope.enter(this);
+            try {
+                preferenceListener.injectPreferenceViews();
+            } finally {
+                scope.exit(this);
+            }
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        eventManager.fire(new OnRestartEvent());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        eventManager.fire(new OnStartEvent());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        eventManager.fire(new OnResumeEvent());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        eventManager.fire(new OnPauseEvent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent ) {
+        super.onNewIntent(intent);
+        eventManager.fire(new OnNewIntentEvent());
+    }
+
+    @Override
+    protected void onStop() {
+        try {
+            eventManager.fire(new OnStopEvent());
+        } finally {
+            super.onStop();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        try {
+            eventManager.fire(new OnDestroyEvent());
+        } finally {
+            try {
+                TriainaInjectorFactory.destroyInjector(this);
+            } finally {
+                super.onDestroy();
+            }
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        final Configuration currentConfig = getResources().getConfiguration();
+        super.onConfigurationChanged(newConfig);
+        eventManager.fire(new OnConfigurationChangedEvent(currentConfig, newConfig));
+    }
+
+    @Override
+    public void onContentChanged() {
+        super.onContentChanged();
+        TriainaInjectorFactory.getInjector(this).injectViewMembers(this);
+        eventManager.fire(new OnContentChangedEvent());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        eventManager.fire(new OnActivityResultEvent(requestCode, resultCode, data));
+    }
+}
