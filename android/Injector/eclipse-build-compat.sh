999d1d8300fe1104abceb715e770e14986d1d303
Vishal Nayak <nayak.vishal@mixi.co.jp>
2014-05-21 17:47:41 +0900

    Error handling added to shell script

--- android/Injector/eclipse-build-compat.sh
+++ android/Injector/eclipse-build-compat.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Execute this shell script only for Eclipse build
 # DO NOT execute for gradle build !!!
@@ -10,9 +10,17 @@ ECLIPSE_CUSTOM_LIBS_DIR="eclipse-libs"
 
 
 # Move all libs under custom eclipse libs to default libs for ANT build
-for file in `ls $ECLIPSE_CUSTOM_LIBS_DIR`
-do
-     cp $ECLIPSE_CUSTOM_LIBS_DIR/$file $LIBS_DIR
-     echo "$file has been re-located for eclipse build of module ==>  $MODULE_NAME"
-     echo "DO NOT commit this change to the GIT repository"
-done
+if [ -d ${ECLIPSE_CUSTOM_LIBS_DIR} ]; then
+    for file in `ls $ECLIPSE_CUSTOM_LIBS_DIR`
+    do
+        if [ -d ${LIBS_DIR} ]; then
+            cp $ECLIPSE_CUSTOM_LIBS_DIR/$file $LIBS_DIR
+            echo "$file has been re-located for eclipse build of module ==>  $MODULE_NAME"
+            echo "DO NOT commit this change to the GIT repository"
+        else
+            echo "Destination directory \"$LIBS_DIR\" missing. Please check your project structure"
+        fi
+    done
+else
+    echo "Source directory \"$ECLIPSE_CUSTOM_LIBS_DIR\" missing. Please check your project structure"
+fi
