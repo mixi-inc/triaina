8a90f2dc3a7ca3eb20a362b1b7ff6edbb43202ad
Yuya Hasegawa <yuya.hasegawa@mixi.co.jp>
2014-10-23 15:25:47 +0900

    add SecurityRuntimeException Resolver interface

--- android/WebViewBridge/tests/java/triaina/test/webview/WebViewClientProxyTest.java
+++ android/WebViewBridge/tests/java/triaina/test/webview/WebViewClientProxyTest.java
@@ -1,33 +1,70 @@
 package triaina.test.webview;
+
+import android.webkit.WebViewClient;
+
+import junit.framework.TestCase;
+
+import triaina.commons.exception.SecurityRuntimeException;
+import triaina.webview.WebViewBridge.SecurityRuntimeExceptionResolver;
 import triaina.webview.WebViewBridge.WebViewClientProxy;
 import triaina.webview.config.DomainConfig;
-import junit.framework.TestCase;
-import android.webkit.WebViewClient;
 
 public class WebViewClientProxyTest extends TestCase {
     private DomainConfig mConfig;
-    
-	@Override
+
+    @Override
     protected void setUp() throws Exception {
         super.setUp();
         mConfig = new DomainConfig(new String[]{"mixi.jp", "mixi.co.jp"});
     }
 
     public void testOnPageStarted() {
-		WebViewClient client = new WebViewClientProxy(new WebViewClient(), mConfig);
-		client.onPageStarted(null, "http://mixi.jp", null);
-		client.onPageStarted(null, "http://mixi.co.jp", null);
-		client.onPageStarted(null, "http://t.mixi.jp", null);
-		client.onPageStarted(null, "http://t.mixi.co.jp", null);
-		
-	}
-	
-	public void testOnPageStartedOnException() {
-		try {
-			WebViewClient client = new WebViewClientProxy(new WebViewClient(), mConfig);
-			client.onPageStarted(null, "http://mix.jp", null);
-			fail();
-		} catch (Exception exp) {
-		}
-	}
+        WebViewClient client = new WebViewClientProxy(new WebViewClient(), mConfig);
+        client.onPageStarted(null, "http://mixi.jp", null);
+        client.onPageStarted(null, "http://mixi.co.jp", null);
+        client.onPageStarted(null, "http://t.mixi.jp", null);
+        client.onPageStarted(null, "http://t.mixi.co.jp", null);
+
+    }
+
+    public void testOriginalOnPageStartedOnException() {
+        try {
+            WebViewClient client = new WebViewClientProxy(new WebViewClient(), mConfig);
+            client.onPageStarted(null, "http://mix.jp", null);
+            fail();
+        } catch (SecurityRuntimeException e) {
+            assertTrue(true);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    public void testOnPageStartedOnExceptionWithoutExceptionResolver() {
+        try {
+            WebViewClient client = new WebViewClientProxy(new WebViewClient(), mConfig, null);
+            client.onPageStarted(null, "http://mix.jp", null);
+            fail();
+        } catch (SecurityRuntimeException e) {
+            assertTrue(true);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    public void testOnPageStartedOnExceptionHandlingWithExceptionResolver() {
+        try {
+            SecurityRuntimeExceptionResolver resolver = new SecurityRuntimeExceptionResolver() {
+                @Override
+                public void resolve(SecurityRuntimeException e) {
+                    assertTrue(true);
+                }
+            };
+            WebViewClient client = new WebViewClientProxy(new WebViewClient(), mConfig, resolver);
+            client.onPageStarted(null, "http://mix.jp", null);
+        } catch (SecurityRuntimeException e) {
+            fail();
+        } catch (Exception e) {
+            fail();
+        }
+    }
 }
